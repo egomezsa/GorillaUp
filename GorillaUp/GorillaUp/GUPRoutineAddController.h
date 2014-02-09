abda4b309ecc98d386d67fe695a75b1b3374d9f7
akawer <aaronkawer@gmail.com>
2014-02-08 19:19:13 -0600

    Un mensaje

---
+++ GorillaUp/GorillaUp/GUPRoutineAddController.h
@@ -0,0 +1,14 @@
+//
+//  GUPRoutineAddController.h
+//  GorillaUp
+//
+//  Created by Aaron Kawer on 2/8/14.
+//  Copyright (c) 2014 BM1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GUPViewController.h"
+
+@interface GUPRoutineAddController : GUPViewController
+
+@end
---
+++ GorillaUp/GorillaUp/GUPRoutineAddController.m
@@ -0,0 +1,29 @@
+//
+//  GUPRoutineAddController.m
+//  GorillaUp
+//
+//  Created by Aaron Kawer on 2/8/14.
+//  Copyright (c) 2014 BM1. All rights reserved.
+//
+
+#import "GUPRoutineAddController.h"
+
+@interface GUPRoutineAddController ()
+
+@end
+
+@implementation GUPRoutineAddController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
+
